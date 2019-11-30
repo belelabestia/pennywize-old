f48f5f14a7f62df2d26cfc9611fb14e5cefa7c60
Marco Jasone Bellezza <jasone.mb@outlook.com>
2019-12-01 00:21:19 +0100

    Creato dbcontext

---
+++ PennywizeServer/Models/PennywizeContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace PennywizeServer.Models
+{
+    public partial class PennywizeContext : DbContext
+    {
+        public PennywizeContext() {}
+
+        public PennywizeContext(DbContextOptions<PennywizeContext> options) : base(options) {}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Data source=pennywize.db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+        public DbSet<Transaction> Transaction { get; set; }
+    }
+}
