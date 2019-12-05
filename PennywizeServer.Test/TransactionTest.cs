9287317dc63f06f8e1ac409a317ed993540a1d72
Marco Jasone Bellezza <jasone.mb@outlook.com>
2019-12-05 16:02:22 +0100

    Refactoring e aggiunta test transaction put

--- PennywizeServer.Test/TransactionTest.cs
+++ PennywizeServer.Test/TransactionTest.cs
@@ -6,6 +6,7 @@ using Xunit;
 using System.Threading.Tasks;
 using System.Linq;
 using Xunit.Abstractions;
+using System.Collections.Generic;
 
 namespace PennywizeServer.Test
 {
@@ -16,49 +17,103 @@ namespace PennywizeServer.Test
         [Fact]
         public async Task GetTransactions_ShouldReturnAll()
         {
-            using (var context = new PennywizeContext(options))
+            IEnumerable<Transaction> transactions0 = new Transaction[0];
+
+            await Using(async (context, controller) =>
             {
-                var controller = new TransactionsController(context);
-                var transactions = (await controller.GetTransaction()).Value;
+                transactions0 = (await controller.GetTransaction()).Value;
+            });
 
-                Assert.True(transactions.Count() == 4);
+            Using((context, controller) =>
+            {
+                var transactions1 = context.Transaction;
 
-                Assert.Collection(transactions.Select(t => t.Description), new Action<string>[]
-                {
-                    d => Assert.True(d == "giochi per bambini"),
-                    d => Assert.True(d == "giochi per adulti"),
-                    d => Assert.True(d == "spotify"),
-                    d => Assert.True(d == "agosto")
-                });
+                Assert.True(transactions1.Count() == 4);
 
-                Assert.Collection(transactions.Select(t => t.Amount), new Action<double>[]
+                Assert.All(transactions0.Zip(transactions1), tt => Assert.True(tt.First.Description == tt.Second.Description));
+
+                Assert.Collection(transactions1.Select(t => t.Amount), new Action<double>[]
                 {
                     a => Assert.True(a == -50),
                     a => Assert.True(a == -500),
                     a => Assert.True(a == -15),
                     a => Assert.True(a == 1500)
                 });
-            }
+            });
         }
 
         [Fact]
-        public async Task PostTransactions_ShouldInsertElement()
+        public async Task GetTranslation_ShouldReturnById()
         {
-            using (var context = new PennywizeContext(options))
+            var transaction0 = new Transaction();
+
+            Using((context, controller) =>
             {
-                var controller = new TransactionsController(context);
+                transaction0 = context.Transaction.First();
+            });
+
+            await Using(async (context, controller) =>
+            {
+                var transaction1 = (await controller.GetTransaction(transaction0.Id)).Value;
+
+                Assert.True(
+                    transaction1.Amount == transaction0.Amount &&
+                    transaction1.Date == transaction0.Date &&
+                    transaction1.Type == transaction0.Type &&
+                    transaction1.Description == transaction0.Description
+                );
+            });
+        }
+
+        [Fact]
+        public async Task PostTransaction_ShouldInsertElement()
+        {
+            await Using(async (context, controller) =>
+            {
                 await controller.PostTransaction(TestTransaction());
+            });
 
-            }
-
-            using (var context = new PennywizeContext(options))
+            await Using(async (context, controller) => await Task.Run(() =>
             {
                 var transactions = context.Transaction;
+                var test = TestTransaction();
 
-                Assert.True(transactions.Count() == 5);
-                Assert.True(transactions.Any(t => t.Description == "tua mamma"));
-                Assert.True(transactions.Any(t => t.Amount == 500));
-            }
+                Assert.True(
+                    transactions.Count() == 5 &&
+                    transactions.Any(t => 
+                        t.Description == test.Description &&
+                        t.Amount == test.Amount
+                    )
+                );
+            }));
+        }
+
+        [Fact]
+        public async Task PutTransaction_ShouldUpdateElement()
+        {
+            var transaction0 = new Transaction();
+            var suffix = " - Changed";
+
+            await Using(async (context, controller) =>
+            {
+                transaction0 = context.Transaction.First();
+
+                transaction0.Amount += 10;
+                transaction0.Description += suffix;
+
+                await controller.PutTransaction(transaction0.Id, transaction0);
+            });
+
+            await Using(async (context, controller) =>
+            {
+                var transaction1 = await context.Transaction.FindAsync(transaction0.Id);
+
+                Assert.True(
+                    transaction1.Id == transaction0.Id &&
+                    transaction1.Amount == transaction0.Amount &&
+                    transaction1.Description == transaction0.Description
+                );
+            });
         }
 
         private Transaction TestTransaction() => new Transaction
@@ -68,5 +123,23 @@ namespace PennywizeServer.Test
             Type = "benzina",
             Description = "tua mamma"
         };
+
+        private async Task Using(Func<PennywizeContext, TransactionsController, Task> action)
+        {
+            using (var context = new PennywizeContext(options))
+            {
+                var controller = new TransactionsController(context);
+                await action(context, controller);
+            }
+        }
+
+        private void Using(Action<PennywizeContext, TransactionsController> action)
+        {
+            using (var context = new PennywizeContext(options))
+            {
+                var controller = new TransactionsController(context);
+                action(context, controller);
+            }
+        }
     }
 }
