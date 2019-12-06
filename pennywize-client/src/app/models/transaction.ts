21d1c3c156ac0ce1dc402e470124d7f628947856
Marco Jasone Bellezza <jasone.mb@outlook.com>
2019-12-06 20:55:13 +0100

    Transaction può essere costruita a partire da un suo partial

--- pennywize-client/src/app/models/transaction.ts
+++ pennywize-client/src/app/models/transaction.ts
@@ -1,7 +1,11 @@
 export class Transaction {
-    id: string;
-    date: Date;
-    amount: number;
-    type: string;
-    description: string;
+  id: string;
+  date: Date;
+  amount: number;
+  type: string;
+  description: string;
+
+  constructor(partial?: Partial<Transaction>) {
+    Object.assign(this, partial);
+  }
 }
