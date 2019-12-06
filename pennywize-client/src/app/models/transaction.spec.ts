bcc0dfe70c69b8cbc932624d0197dd9f480c54a8
Marco Jasone Bellezza <jasone.mb@outlook.com>
2019-12-06 21:04:28 +0100

    Aggiunti test model

--- pennywize-client/src/app/models/transaction.spec.ts
+++ pennywize-client/src/app/models/transaction.spec.ts
@@ -4,4 +4,22 @@ describe('Transaction', () => {
   it('should create an instance', () => {
     expect(new Transaction()).toBeTruthy();
   });
+
+  it('should create an instance with partial', () => {
+    const partial = {
+      id: '0u9gu0g4er3',
+      amount: 2305,
+      description: '53082rhfe'
+    };
+
+    const transaction = new Transaction(partial);
+
+    expect(transaction).toBeTruthy();
+
+    expect(
+      transaction.id == partial.id &&
+      transaction.amount == partial.amount &&
+      transaction.description == partial.description
+    ).toBeTruthy();
+  });
 });
