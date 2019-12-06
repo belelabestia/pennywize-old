4767da3906083d4445392bf733a39010cd788260
Marco Jasone Bellezza <jasone.mb@outlook.com>
2019-12-06 20:55:30 +0100

    Scritti e passati i test del TransactionService

--- pennywize-client/src/app/services/transaction.service.spec.ts
+++ pennywize-client/src/app/services/transaction.service.spec.ts
@@ -1,12 +1,87 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { TransactionService } from './transaction.service';
+import { Transaction } from '../models/transaction';
 
 describe('TransactionService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let httpTestingController: HttpTestingController;
+  let transactionService: TransactionService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+
+    httpTestingController = TestBed.get(HttpTestingController);
+    transactionService = TestBed.get(TransactionService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
 
   it('should be created', () => {
-    const service: TransactionService = TestBed.get(TransactionService);
-    expect(service).toBeTruthy();
+    expect(transactionService).toBeTruthy();
+  });
+
+  it('should get transactions', () => {
+    const transactions: Transaction[] = [
+      new Transaction({
+        id: 'gubh78yb79bt',
+        amount: 340,
+        date: new Date(),
+        type: 'benzina',
+        description: 'esfpjpigvp'
+      }),
+      new Transaction({
+        id: 'fhush8werh',
+        amount: 340,
+        date: new Date(),
+        type: 'svago',
+        description: 'fheshesiohfs'
+      }),
+      new Transaction({
+        id: '78tb7tbt87tn',
+        amount: 340,
+        date: new Date(),
+        type: 'bollette',
+        description: 'gas'
+      }),
+    ];
+
+    transactionService.transactions.subscribe(
+      tt => expect(tt).toEqual(transactions)
+    );
+
+    transactionService.get();
+
+    const req = httpTestingController.expectOne('api/transactions');
+    expect(req.request.method).toBe('GET');
+    req.flush(transactions);
+  });
+
+  it('should post a transaction', () => {
+    const transaction = new Transaction({
+      id: 'fhtc5ueyc89ny',
+      amount: 843,
+      date: new Date(),
+      type: 'y49w8tgh',
+      description: 'dasjifo8hfh8f'
+    });
+
+    transactionService.transactions.subscribe(tt =>
+      expect(tt).toContain(transaction)
+    );
+
+    transactionService.post(transaction);
+
+    const req0 = httpTestingController.expectOne('api/transactions');
+    expect(req0.request.method).toBe('POST');
+    req0.flush(transaction);
+
+    const req1 = httpTestingController.expectOne('api/transactions');
+    expect(req0.request.method).toBe('POST');
+    req1.flush([transaction]);
   });
 });
