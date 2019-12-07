5c9d373af55f86ba89bd20ba34bbc05ab2081373
Marco Jasone Bellezza <jasone.mb@outlook.com>
2019-12-08 00:53:58 +0100

    Format transaction.cs

--- PennywizeServer/Models/Transaction.cs
+++ PennywizeServer/Models/Transaction.cs
@@ -6,10 +6,10 @@ namespace PennywizeServer
     public class Transaction
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public string Id {get;set;}
-        public DateTime Date {get;set;}
-        public double Amount {get;set;}
-        public string Type {get;set;}
-        public string Description {get;set;}
+        public string Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
     }
 }
