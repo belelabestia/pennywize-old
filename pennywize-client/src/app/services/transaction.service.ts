fd389ead8c0bffce94f31f60e0644b4c8c845523
Marco Jasone Bellezza <jasone.mb@outlook.com>
2019-12-08 15:04:06 +0100

    Aggiunto catch nelle chiamate in transaction service

--- pennywize-client/src/app/services/transaction.service.ts
+++ pennywize-client/src/app/services/transaction.service.ts
@@ -15,16 +15,22 @@ export class TransactionService {
   constructor(private hc: HttpClient) { }
 
   get(): Promise<void> {
-    return new Promise(resolve => this.hc.get<Transaction[]>(url).subscribe(tt => {
-      this.transactionsSub.next(tt);
-      resolve();
-    }));
+    return new Promise((resolve, reject) => this.hc.get<Transaction[]>(url).subscribe(
+      tt => {
+        this.transactionsSub.next(tt);
+        resolve();
+      },
+      err => reject()
+    ));
   }
 
   post(t: Transaction): Promise<void> {
-    return new Promise(resolve => this.hc.post(url, t).subscribe(() => {
-      this.get();
-      resolve();
-    }));
+    return new Promise((resolve, reject) => this.hc.post(url, t).subscribe(
+      () => {
+        this.get();
+        resolve();
+      },
+      err => reject()
+    ));
   }
 }
