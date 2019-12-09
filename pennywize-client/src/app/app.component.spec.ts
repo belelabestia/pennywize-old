3da836e2f7848556eadc07a083eda5009fb97bbf
Marco Jasone Bellezza <jasone.mb@outlook.com>
2019-12-09 20:14:35 +0100

    Aggiunta gestione errori

--- pennywize-client/src/app/app.component.spec.ts
+++ pennywize-client/src/app/app.component.spec.ts
@@ -1,8 +1,13 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ErrorService } from './services/error.service';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let service: ErrorService;
+  let component: AppComponent;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -12,11 +17,21 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    service = TestBed.get(ErrorService);
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('should receive error', async () => {
+    service.dispatch('test error');
+    await fixture.whenStable();
+    expect(component.errorMessage).toBe('test error');
   });
 });
--- pennywize-client/src/app/app.component.ts
+++ pennywize-client/src/app/app.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { ErrorService } from './services/error.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+  errorMessage: string;
+  subscription: Subscription;
+
+  constructor(private e: ErrorService) { }
+
+  ngOnInit() {
+    this.subscription = this.e.error.subscribe(error => { this.errorMessage = error.message; });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  dismissError() {
+    this.errorMessage = null;
+  }
+}
