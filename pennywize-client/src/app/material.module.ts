7ef29edbb3795ddae5985cb7ea3cd04ff8daf8e0
Marco Jasone Bellezza <jasone.mb@outlook.com>
2019-12-15 01:49:02 +0100

    Avanzamenti stilizzazione componenti interni

--- pennywize-client/src/app/material.module.ts
+++ pennywize-client/src/app/material.module.ts
@@ -4,18 +4,28 @@ import {
   MatCardModule,
   MatButtonModule,
   MatIconModule,
-  MatInputModule
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatFormFieldModule,
+  MAT_DATE_LOCALE
 } from '@angular/material';
 
 const modules = [
   MatCardModule,
   MatButtonModule,
   MatIconModule,
-  MatInputModule
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatFormFieldModule
 ];
 
 @NgModule({
   imports: modules,
-  exports: modules
+  exports: modules,
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'it-IT'}
+  ]
 })
 export class MaterialModule { }
