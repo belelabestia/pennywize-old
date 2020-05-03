cdc275d6d7bcdb18b50b119f41f95595bf298064
Marco Jasone Bellezza <jasone.mb@outlook.com>
2020-05-03 03:28:49 +0200

    Legale tutto insieme

---
+++ pennywize-client/src/app/components/legal/terms-and-conditions/terms-and-conditions.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terms-and-conditions',
+  templateUrl: './terms-and-conditions.component.html',
+  styleUrls: ['./terms-and-conditions.component.css', '../legal.css']
+})
+export class TermsAndConditionsComponent { }
