e4fc3870fa57f1b857f9bc67b1e9a69288b9a58e
Marco Jasone Bellezza <jasone.mb@outlook.com>
2020-05-16 23:48:34 +0200

    Fix auth conf redirect

--- pennywize-client/src/app/auth/auth.conf.ts
+++ pennywize-client/src/app/auth/auth.conf.ts
@@ -1,10 +1,11 @@
 import { AuthConf } from './interfaces';
+import { environment } from 'src/environments/environment';
 
 export const authConf: AuthConf = {
   issuer: 'https://accounts.google.com',
   responseType: 'code',
   clientId: '748180026787-gnbgs0f358t6qq5v9ph8aanovq39pkee.apps.googleusercontent.com',
-  redirectUri: location.origin,
+  redirectUri: environment.production ? 'https://pwz.belelabestia.it' : 'http://localhost:4200',
   scope: 'openid profile email',
   clientSecret: 'H5DIRTbc0vRD-JGp2Le0a2Z0',
   apiPath: 'api/',
